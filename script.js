538db85a531aba9c715fb6c211e51722025d37fc
Dev <Gabrieldruel.dev@gmail.com>
2022-03-02 01:11:45 +0100

    header page d'accueil terminer
    fonction close
    fonction open au click du button en js 
    header contenant le logo au click la liste Vien se poser devant avec une animation css  venant de la gauche ouvrant une liste de menu  le tous penser mobile

---
+++ script.js
@@ -0,0 +1,12 @@
+function openlist(){
+    document.getElementById("ContentMenu").style.left = "0";
+    document.getElementById("ContentMenu").style.opacity = "1";
+}
+function closelist(){
+    document.getElementById("ContentMenu").style.left = "-100%";
+    document.getElementById("ContentMenu").style.opacity = "0";
+}
+  
+
+  
+
